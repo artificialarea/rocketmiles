03d03a62dfdd4a7efeb43c7fc6153d63c55f1a1a
Sacha Sedriks <sachased@gmail.com>
2021-04-18 08:22:21 -0700

    Add various types of state and controlled filter inputs
    Add state for filter name search and sort types with controlled components. Two types of state for hotels: master and display. Display state adjusted by filter input. Filter hotel name search and reset functionality in place.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,19 +6,59 @@ import Filters from '../Filters/Filters';
 import HotelList from '../Hotels/HotelList';
 
 const App = () => {
-    const [hotels, setHotels] = useState([]);
+    const [hotelMaster, setHotelMaster] = useState([]);
+    const [hotelDisplay, setHotelDisplay] = useState([]);
+    const [searchHotels, setSearchHotels] = useState('');
+    const [sortHotels, setSortHotels] = useState('recommended');
 
     useEffect(() => {
         hotelResultService.get().then(response => {
-            setHotels(response.results.hotels)
+            setHotelMaster(response.results.hotels);
+            setHotelDisplay(response.results.hotels);
         })
     }, []);
+
+    const filterHotels = (name) => {
+        if (name) {
+            let filteredList = hotelMaster.filter(hotel => {
+                return hotel.hotelStaticContent.name.toLowerCase().includes(name);
+            });
+            setHotelDisplay(filteredList);
+        } else {
+            setHotelDisplay(hotelMaster);
+        }
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const { value } = e.target;
+        setSearchHotels(value);
+        filterHotels(value);
+    }
+
+    const handleSort = (e) => {
+        e.preventDefault();
+        const { value } = e.target;
+        setSortHotels(value);
+    }
+
+    const handleReset = () => {
+        setSortHotels('recommended');
+        setSearchHotels('');
+        filterHotels();
+    }
 
     return (
         <div className="app-container">
             <div className="content">
-                <Filters />
-                <HotelList hotels={hotels} />
+                <Filters 
+                    searchHotels = {searchHotels}
+                    sortHotels = {sortHotels}
+                    onSearch={handleSearch}
+                    onSort={handleSort}
+                    onReset={handleReset}
+                />
+                <HotelList hotels={hotelDisplay} />
             </div>
         </div>
     )
--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,17 +1,43 @@
 import React from 'react';
 
 export default function Filters(props) {
+    const { 
+        searchHotels, 
+        sortHotels,
+        onSearch,
+        onSort, 
+        onReset,
+    } = props;
+
+    console.log('searchHotels:', searchHotels);
+    console.log('sortHotels:', sortHotels);
+
     return (
         <div className="filters">
             Hotel name
-            <input type="text" className="input" maxLength={1}/>
-            Price
-            <select name="" className="select">
-                <option value="">Recommended</option>
-                <option value="">Price low-to-high</option>
-                <option value="">Price high-to-low</option>
+            <input 
+                type="text" 
+                className="input" 
+                value={searchHotels}
+                onChange={(e) => onSearch(e)}
+            />
+            Sort
+            <select 
+                name="" 
+                className="select"
+                value={sortHotels} 
+                onChange={(e) => onSort(e)}
+            >
+                <option value="recommended">Recommended</option>
+                <option value="low-price">Price low-to-high</option>
+                <option value="high-price">Price high-to-low</option>
             </select>
-            <button className="button">Reset</button>
+            <button 
+                className="button"
+                onClick={() => onReset()}
+            >
+            Reset
+            </button>
         </div>
     )
 }
